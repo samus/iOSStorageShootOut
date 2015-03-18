4f00a2a6dea35986985e8b5624ee33e4689b8caf
Sam Corder <samus@codeargyle.com>
2015-03-17 23:51:46 -0400

    Initial commit with YAPDatabase and FMDB project shells.

---
+++ FMDBShootOut/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FMDBShootOut
+//
+//  Created by Sam Corder on 3/17/15.
+//  Copyright (c) 2015 Synapptic Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+
+workspace 'ShootOut'
+
+target 'YAPShootOut' do
+	pod 'YapDatabase', '~> 2.5'
+end
+
+target 'YAPShootOutTests' do
+
+end
+
+target 'FMDBShootOut' do
+	pod 'FMDB', '~> 2.5'
+end
+
+# target 'RLiteShootOut' do
+# 	pod 'objc-rlite', '~> 0.0'
+# end
